a87b6530a6a98fe684d884177d7899e40f42627a
Florian Chédemail <florian.chedemail@gmail.com>
2016-03-05 11:42:59 +0100

    this may work better

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -43,8 +43,6 @@ Route::group(
     }
 );
 
-Route::group(['prefix' => 'api', 'middleware' => 'auth:api'], function () {
-    Route::post('signin', ['uses' => 'ApiController@signin']);
-});
+Route::post('api/signin', ['uses' => 'ApiController@signin']);
 
 Route::get("api/randomApiToken", ['uses' => 'ApiController@randomApiToken']);
