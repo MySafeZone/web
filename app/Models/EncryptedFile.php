c7827b8711872a6cabf5cce544718fa44fce6aaf
Florian Chédemail <florian.chedemail@gmail.com>
2016-03-05 18:18:10 +0100

    fuck save

--- app/Models/EncryptedFile.php
+++ app/Models/EncryptedFile.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\UuidModel;
 
-class EncryptedFile extends Model
+class EncryptedFile extends UuidModel
 {
-    //
+    public $incrementing = false;
+    public $timestamps = true;
+    protected $table = 'encryptedfiles';
+
+    public function user()
+    {
+        return $this->belongsTo('User'); 
+    }
 }
