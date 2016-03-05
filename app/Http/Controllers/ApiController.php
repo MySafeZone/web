b47ffa24674b0cb02a5e9f3e2c6b3c79c6bdd505
Florian Chédemail <florian.chedemail@gmail.com>
2016-03-05 11:11:31 +0100

    forgotten traits

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Auth;
+
 class ApiController extends Controller
 {
 	public function signin(Request $request)
